b0106d3e8e78148e8a1119c9181329dd8840fbd8
Michael Soares <mikey.soares@gmail.com>
2018-10-21 21:53:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function placeOrder(cardNumber) {
   if (cardNumber.length === 0) {
   return "Sorry, we don't have a credit card on file for you."
 }
-
+cart = []
 var totalPrice = total()
 return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
