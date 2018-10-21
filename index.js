0bfad48e6ca83a0dfd7fa790cfdd62a2da18189f
Michael Soares <mikey.soares@gmail.com>
2018-10-21 21:27:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,45 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
- var price = (Math.floor(Math.random() * 101));
- var cart = `itemName: ${item[i]}, itemPrice: ${price[i]}`
-
- return cart
+ // rite your code here
+ var itemName = {itemName: item};
+ var price = Math.floor(Math.random() * 100) + 1;
+ var prices = {itemPrice: price};
+const totalItem = Object.assign(itemName, prices);
+ cart.push(totalItem);
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    return `Your shopping cart is empty.`
+  }
+  var message =[];
+  for (var i = 0; i < cart.length; i++) {
+    message.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+  }
+  if (cart.length === 1){
+    return `In your cart, you have ${message}.`;
+  }
+  else if (cart.length === 2) {
+    return `In your cart, you have ${message.join(', and ')}.`;
+  }
+  else{
+    var finalItem = message.pop()
+    return `In your cart, you have ${message.join(', ')}, and ${finalItem}.`;
+  }
 }
+
+
 
 function total() {
   // write your code here
+  var priceList = []
+  for (var i = 0; i < cart.length; i++) {
+    priceList.push(parseInt(cart[1].itemPrice));
+  }
+  var totalPrice = itemPrice.sum()
+  return totalPrice
 }
 
 function removeFromCart(item) {
