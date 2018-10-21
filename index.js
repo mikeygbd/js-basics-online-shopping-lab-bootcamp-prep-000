5068fb1a7664b8a22579f1b2593f66c58e790e43
Michael Soares <mikey.soares@gmail.com>
2018-10-21 21:48:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,4 +67,5 @@ function placeOrder(cardNumber) {
   if (cardNumber.length === 0) {
   return "Sorry, we don't have a credit card on file for you."
 }
+return `Your total cost is $${total}, which will be charged to the card 83296759.`
 }
