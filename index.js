5c53a7f9a70e044c8ba4404e3c679d8236fbac4c
Michael Soares <mikey.soares@gmail.com>
2018-10-21 21:59:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ return "That item is not in your cart."
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber.length === undefined) {
+  if (cardNumber === undefined) {
   return "Sorry, we don't have a credit card on file for you."
 } else {
   var totalPrice = total()
