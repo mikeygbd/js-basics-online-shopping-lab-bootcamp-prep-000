fbf237708df6f00995863ad0d9aafb8f517d45b5
Michael Soares <mikey.soares@gmail.com>
2018-10-21 21:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,13 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  item.pop()
+for (var i = 0; i < cart.length; i++){
+  if (cart[i].itemName === item){
+    cart.splice(i, 1);
+    return cart
+  }
+}
+return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
